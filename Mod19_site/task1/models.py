63f9d231637224fed21813fa3e9742cab3a07a0a
murad <moro11@mail.ru>
2024-10-17 00:11:30 +0300

    17.10.2024 - Update

--- Mod19_site/task1/models.py
+++ Mod19_site/task1/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=300)
-    hint = models.TextField()
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
