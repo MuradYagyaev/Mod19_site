a8db48fc06614140e9ad7c79026b8cb9be3984ed
murad <moro11@mail.ru>
2024-10-19 23:18:24 +0300

    19.10.2024 - Pagination

--- Mod19_site/task1/views.py
+++ Mod19_site/task1/views.py
@@ -10,7 +10,7 @@ def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     perpage = request.GET.get('per_page')
     if perpage is None:
-        perpage = 5
+        perpage = str(5)
     page_number = request.GET.get('page')
     paginator = Paginator(posts, per_page=perpage)
     page_posts = paginator.get_page(page_number)
